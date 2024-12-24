716d7421fe2c1f282ecd7269cc1c1f955c47b38c
mohit0224 <mohitdheer24+git@gmail.com>
2024-12-24 16:34:50 +0530

    feat: integrate Prettier for code formatting and update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,20 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config({
-	languageOptions: {
-		parserOptions: {
-			project: true,
-			tsconfigRootDir: import.meta.dirname,
-		},
-	},
-	files: ["**/*.ts"],
-	extends: [
-		eslint.configs.recommended,
-		...tseslint.configs.recommendedTypeChecked,
-	],
-	rules: {
-		"no-console": "error",
-		quotes: ["error", "single", { allowTemplateLiterals: true }],
-	},
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
+    },
+    files: ["**/*.ts"],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, eslintConfigPrettier],
+    rules: {
+        "no-console": "error",
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+    },
 });
+
