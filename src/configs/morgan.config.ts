90fa51ef6a16f75be25559554694073477fdfde0
mohit0224 <mohitdheer24+git@gmail.com>
2024-12-25 13:17:52 +0530

    feat: include client IP address in morgan logging format

--- src/configs/morgan.config.ts
+++ src/configs/morgan.config.ts
@@ -1,16 +1,17 @@
 import logger from "../utils/logger.utils";
 
-export const morganFormat = ":method :url :status :response-time ms";
+export const morganFormat = ":remote-addr :method :url :status :response-time ms";
 
 export const morganFnc = {
     stream: {
         write: (message: unknown) => {
             if (typeof message === "string") {
                 const logObject = {
-                    method: message.split(" ")[0],
-                    url: message.split(" ")[1],
-                    status: message.split(" ")[2],
-                    responseTime: message.split(" ")[3],
+                    method: message.split(" ")[1],
+                    url: message.split(" ")[2],
+                    status: message.split(" ")[3],
+                    responseTime: message.split(" ")[4] + " " + message.split(" ")[5],
+                    ip: message.split(" ")[0],
                 };
 
                 const statusCode = Number(logObject.status);
