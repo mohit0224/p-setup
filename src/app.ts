9e28c431fd041ad4894c8081948384ff842ef544
mohit0224 <mohitdheer24+git@gmail.com>
2024-12-25 09:08:46 +0530

    feat: enhance root route response with structured API response and request details

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Response } from "express";
+import express, { Application } from "express";
 import path from "path";
 import globalErrorHandler from "./middlewares/globalErrorHandler.middleware";
+import asyncHandler from "./utils/asyncHandler.utils";
+import { apiResponse } from "./utils/httpResponse.utils";
 
 const app: Application = express();
 
@@ -9,9 +11,12 @@ app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (_, res: Response) => {
-    res.send("Hello, World!");
-});
+app.get(
+    "/",
+    asyncHandler((req, res) => {
+        res.status(200).json(new apiResponse(200, "Welcome to the API", "", req));
+    })
+);
 
 app.use(globalErrorHandler);
 
--- src/utils/httpResponse.utils.ts
+++ src/utils/httpResponse.utils.ts
@@ -1,14 +1,26 @@
-export class apiResponse<T = unknown> {
+import { Request } from "express";
+
+export class apiResponse<T> {
     status: number;
     message: string;
     success: boolean;
     data?: T;
+    request?: {
+        ip: string;
+        method: string;
+        url: string;
+    };
 
-    constructor(statusCode: number, message = "Success", data?: T) {
+    constructor(statusCode: number, message = "Success", data?: T, req?: Request) {
         this.status = statusCode;
         this.message = message;
         this.success = statusCode < 400;
         this.data = data;
+        this.request = {
+            ip: req?.ip || "",
+            method: req?.method || "",
+            url: req?.originalUrl || "",
+        };
     }
 }
 
@@ -18,12 +30,7 @@ export class apiError extends Error {
     data: null;
     errors: string[];
 
-    constructor(
-        statusCode: number,
-        message = "Something went wrong",
-        errors: string[] = [],
-        stack: string = ""
-    ) {
+    constructor(statusCode: number, message = "Something went wrong", errors: string[] = [], stack: string = "") {
         super(message);
         this.status = statusCode;
         this.message = message;
@@ -38,3 +45,4 @@ export class apiError extends Error {
         }
     }
 }
+
