d732199d35639cba9439bb76a3ed3f772146c370
mohit0224 <mohitdheer24+git@gmail.com>
2024-12-26 15:44:11 +0530

    feat: add rate limiting middleware to enhance API security and prevent abuse

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import healthController from "../controllers/health.controller";
+import limiter from "../middlewares/rateLimiter.middleware";
 const healthRouter = Router();
 
-healthRouter.get("/",healthController.healthCheck);
+healthRouter.get("/", limiter(), healthController.healthCheck);
 
 export default healthRouter;
 
