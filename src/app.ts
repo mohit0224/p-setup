16f2629a70d47212b3944ffd790a95ab9b2a2af7
mohit0224 <mohitdheer24+git@gmail.com>
2024-12-26 14:28:44 +0530

    feat: add helmet middleware for enhanced security and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,14 @@ import { morganFnc, morganFormat } from "./configs/morgan.config";
 import globalErrorHandler from "./middlewares/globalErrorHandler.middleware";
 import logRouter from "./routes/logs.routes";
 import healthRouter from "./routes/health.routes";
+import helmet from "helmet";
+import isHttps from "./middlewares/isHttps.middleware";
 
 const app: Application = express();
 
 app.set("trust proxy", 1);
+app.use(isHttps);
+app.use(helmet());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(express.static(path.join(__dirname, "public")));
